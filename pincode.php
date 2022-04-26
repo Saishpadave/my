15a46cb32cc11242f639f097d14cc80635aba25c
PreetPatil4055 <104415175+PreetPatil4055@users.noreply.github.com>
2022-04-26 12:00:42 +0530

    Add files via upload

---
+++ pincode.php
@@ -0,0 +1,128 @@
+<?php include("header1.php"); 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Let us locate you</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+ <style> 
+   #pincode{
+       width:21%;
+}
+
+
+.textbox{
+  width: 30%;
+  box-sizing: border-box;
+  border: 2px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+  background-position: 10px 10px; 
+  background-repeat: no-repeat;
+  padding: 12px 20px 12px 40px;
+  -webkit-transition: width 0.4s ease-in-out;
+  transition: width 0.4s ease-in-out;
+  margin-left:2%;
+}
+.btn {
+    display: inline-block;
+    font-weight: 400;
+    text-align: center;
+    cursor: pointer;
+    background-image: none;
+    border: 1px solid transparent;
+    padding: 6px 12px;
+    font-size: 14px;
+    border-radius: 4px;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+	height:46px;
+	color: #fff;
+    background-color: #5cb85c;
+    border-color: #4cae4c;
+	margin-left:2%;
+}
+
+.te{
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    font: size 300px;
+    font-style:inherit;
+    font-weight: bold;
+}
+h2{
+    font-family:Arial; 
+    font-size:30px; 
+    text-align:center;
+    }
+</style>
+</head>
+<body class="loc">
+<div class="container">
+	<div><h2 class="te">Help us locate you</h2></div>
+	<div>&nbsp;</div>
+	<form autocomplete="off" method="post" id="frmPinCode" style="text-align:center;">
+		<div>
+			<input type="text" class="textbox" name="pincode" id="pincode" placeholder="Enter Pincode"  autocomplete="new-password">
+			<input type="button" class="btn" value="Get Details" onclick="get_details()">
+		</div>
+		<div>&nbsp;</div>
+		<div>
+			<input type="text" class="textbox" id="city" disabled placeholder="City"><br/><br/>
+			<input type="text" class="textbox" id="state" disabled placeholder="State">
+			
+		</div><br>
+		<div>
+		<input type="button" class="btn"  value="Submit" onclick="checkforblank()">
+		</div>
+	</form>
+</div>
+<script>
+        function checkforblank() {
+            if(document.getElementById('pincode').value == "401404") {
+                alert('your location matched');
+                window.location.href="search.php";
+                /*return false;*/
+            }
+            else if(document.getElementById('pincode').value =="401208") {
+                alert('your location matched');
+                window.location.href="search.php";
+                /*return false;*/
+            }
+            else{
+                alert('Please enter correct location');
+            }
+
+        }
+        </script>
+<script>
+function get_details(){
+	var pincode=jQuery('#pincode').val();
+	if(pincode==''){
+		jQuery('#city').val('');
+		jQuery('#state').val('');
+	}else{
+		jQuery.ajax({
+			url:'get.php',
+			type:'post',
+			data:'pincode='+pincode,
+			success:function(data){
+				if(data=='no'){
+					alert('Wrong Pincode');
+					jQuery('#city').val('');
+					jQuery('#state').val('');
+				}else{
+					var getData=$.parseJSON(data);
+					jQuery('#city').val(getData.city);
+					jQuery('#state').val(getData.state);
+				}
+			}
+		});
+	}
+}
+</script>
+</body>
+</html>
